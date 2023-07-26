6d1e4295c48928b313346406ac10a75a07f4f6aa
aaalbinaaaa <albinarubanchuk@gmail.com>
2023-07-26 21:11:08 +0300

    add header and footer to separate files for reusability

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+<footer>
+	<div class="row">
+		<div class="col-lg-12">
+			<p>Copyright &copy; Your Website
+
+				<?php
+				$date = date("Y");
+				echo $date;
+				?>
+			</p>
+		</div>
+		<!-- /.col-lg-12 -->
+	</div>
+	<!-- /.row -->
+</footer>
--- index.php
+++ index.php
@@ -1,30 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Blog Home - Start Bootstrap Template</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/blog-home.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
+<?php
+include "includes/header.php";
+?>
 
 <body>
 
@@ -61,7 +37,7 @@
     </nav>
 
     <!-- Page Content -->
-    <div class="container">
+    <main class="container">
 
         <div class="row">
 
@@ -144,7 +120,7 @@
                         <span class="input-group-btn">
                             <button class="btn btn-default" type="button">
                                 <span class="glyphicon glyphicon-search"></span>
-                        </button>
+                            </button>
                         </span>
                     </div>
                     <!-- /.input-group -->
@@ -198,17 +174,11 @@
         <hr>
 
         <!-- Footer -->
-        <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-        </footer>
 
-    </div>
+        <?php
+        include "includes/footer.php";
+        ?>
+    </main>
     <!-- /.container -->
 
     <!-- jQuery -->
