807a65b463eb82d9779cc4f9ad21a8f71ddcdd16
aaalbinaaaa <albinarubanchuk@gmail.com>
2023-07-27 16:35:36 +0300

    add displayind data in admin categories from db

--- admin/posts.php
+++ admin/posts.php
@@ -37,17 +37,34 @@ include "includes/admin_header.php";
 								</tr>
 							</thead>
 							<tbody>
-								<tr>
-									<td>10</td>
-									<td>Albina</td>
-									<td>Bootstrap framework</td>
-									<td>Bootstrap</td>
-									<td>Status</td>
-									<td>Image</td>
-									<td>Tags</td>
-									<td>Comments</td>
-									<td>Date</td>
-								</tr>
+								<?php
+								$query = "SELECT * FROM posts";
+								$selectPosts = $mysql->query($query);
+
+								while ($row = $selectPosts->fetch_assoc()) {
+									$postId = $row['post_id'];
+									$postAuthor = $row['post_author'];
+									$postTitle = $row['post_title'];
+									$postCatId = $row['post_category_id'];
+									$postStatus = $row['post_status'];
+									$postImage = $row['post_image'];
+									$postTags = $row['post_tags'];
+									$postCommentCount = $row['post_comment_count'];
+									$postDate = $row['post_date'];
+
+									echo "<tr>";
+									echo "<td>$postId</td>";
+									echo "<td>$postAuthor</td>";
+									echo "<td>$postTitle</td>";
+									echo "<td>$postCatId</td>";
+									echo "<td>$postStatus</td>";
+									echo "<td><img class='img-responsive' src='$postImage' alt='background image' style='max-width: 150px; width: 100%; margin: 0 auto;'/></td>";
+									echo "<td>$postTags</td>";
+									echo "<td>$postCommentCount</td>";
+									echo "<td>$postDate</td>";
+									echo "</tr>";
+								}
+								?>
 							</tbody>
 						</table>
 
