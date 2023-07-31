bb16a9b55a1b70c0cb196f3cee1d2ec25b380f4b
aaalbinaaaa <albinarubanchuk@gmail.com>
2023-07-31 12:40:49 +0300

    created the loging form

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -16,6 +16,26 @@
 		<!-- /.input-group -->
 	</div>
 
+	<!-- Login -->
+	<div class="well">
+		<h4>Login</h4>
+		<form action="includes/login.php" method="post">
+			<div class="form-group">
+				<input type="text" name="username" class="form-control" placeholder="Enter username">
+
+			</div>
+			<div class="input-group">
+				<input type="password" name="password" class="form-control" placeholder="Enter password">
+				<span class="input-group-btn">
+					<button class="btn btn-primary" name="login" type="submit">
+						Submit
+					</button>
+				</span>
+			</div>
+		</form>
+		<!-- /.input-group -->
+	</div>
+
 	<!-- Blog Categories Well -->
 	<div class="well">
 
@@ -41,6 +61,7 @@
 		</div>
 		<!-- /.row -->
 	</div>
+
 
 	<!-- Side Widget Well -->
 
