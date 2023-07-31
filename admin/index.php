2f957da05c57de224abea24e3704f7123470cce8
aaalbinaaaa <albinarubanchuk@gmail.com>
2023-07-31 15:55:09 +0300

    addet an chart to admin

--- admin/index.php
+++ admin/index.php
@@ -147,6 +147,32 @@ include "includes/admin_header.php";
                 </div>
                 <!-- /.row -->
 
+                <div class="row">
+                    <script>
+                        google.charts.load('current', {
+                            packages: ['corechart']
+                        });
+                        google.charts.setOnLoadCallback(drawChart);
+
+                        function drawChart() {
+                            // Define the chart to be drawn.
+                            var data = new google.visualization.DataTable();
+                            data.addColumn('string', 'Element');
+                            data.addColumn('number', 'Percentage');
+                            data.addRows([
+                                ['Nitrogen', 0.78],
+                                ['Oxygen', 0.21],
+                                ['Other', 0.01]
+                            ]);
+
+                            // Instantiate and draw the chart.
+                            var chart = new google.visualization.PieChart(document.getElementById('myPieChart'));
+                            chart.draw(data, null);
+                        }
+                    </script>
+                    <div id="myPieChart" />
+                </div>
+
             </div>
             <!-- /.container-fluid -->
 
