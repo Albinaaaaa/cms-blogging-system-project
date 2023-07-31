3b8dbd514103626c37cf26b1208a6c22fb99a979
aaalbinaaaa <albinarubanchuk@gmail.com>
2023-07-31 13:21:56 +0300

    made select user query

--- includes/login.php
+++ includes/login.php
@@ -8,5 +8,21 @@ include "db.php";
 if (isset($_POST['login'])) {
 	$username = $_POST['username'];
 	$password = $_POST['password'];
+
+	$username = $mysql->real_escape_string($username);
+	$password = $mysql->real_escape_string($password);
+
+	$query = "SELECT * FROM users WHERE username = '{$username}'";
+	$selectUserQuery = $mysql->query($query);
+
+	if (!$selectUserQuery) {
+		die("query failed");
+	}
+
+	while ($row = $selectUserQuery->fetch_array()) {
+		$dbId = $row['user_id'];
+
+		echo $dbId;
+	}
 }
 ?>
